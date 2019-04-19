5531ce1c938a9df0e1058a769291b30a80906522
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2019-04-19 17:17:24 -0500

    gui update

--- GUI.py
+++ GUI.py
@@ -3,11 +3,18 @@ from tkinter import filedialog
 import tkinter as tk
 import os
 from PIL import ImageTk, Image
+import cv2
+import Binary
+from datetime import datetime
 
 
-global img
+
+global inputImage
 global label
-global imageLocation
+global finalLabel
+global imageTypeValue
+global funcType
+
 
 def getImage(self):
     curDir = os.getcwd()
@@ -15,31 +22,112 @@ def getImage(self):
         initialdir = curDir, title = "Select file", 
         filetypes = (("jpeg files","*.jpg *.jpeg"),("png files","*.png")))
 
-    print(imageLocation)
-
+    #this makes the input image into a matrix
+    inputImage = cv2.imread(imageLocation, 0)
+    print(inputImage)
     img = ImageTk.PhotoImage(file = imageLocation)  #Image.open(imageLocation))
-    label = tk.Label(root, image=img).grid(row = 10, column = 0)
+    label = tk.Label(root, image = img).grid(row = 10, columnspan = 6)
+    label.image = img
+
     return 
 
 
-def getImageType(self):
-    a = binaryGreyValue.get()
-    method = StringVar()
-    if a == 2:
-        method.set("greyscale")
-        #call the greyscale function
-    elif a == 1:
-        method.set("binary")
+def getImageType():
+    imageTypeValue = binaryGreyValue.get()
+    print(imageTypeValue)
+    print("changed value")
+    # if imageTypeValue == 2:
+    #     method.set("greyscale")
+    # elif imageTypeValue == 1:
+        # method.set("binary")
 
     #return the new discolored image    
-    return 1
+    return
 
 
 
-def processImage(self, event):
 
-    #method =
-    #description = getDescription(method)
+def getFunctionType():
+    print("changing the function")
+    funcType = functionTypeValue.get()
+    print(funcType)
+    return
+
+
+
+
+def makeGreyOrBinary():
+    if imageTypeValue == 2: #greyscale
+        print("greyscale")
+        #greyscale function on the image
+    elif imageTypeValue == 1:
+        binaryObj = Binary.Binary()
+        
+        histogram = binaryObj.compute_histogram(inputImage)
+        optThreshold = binaryObj.find_optimal_threshold(histogram, inputImage)
+        binaryObj.binarize(inputImage, optThreshold)
+
+
+        print("h")
+
+    return
+
+
+def runFunction():
+    if funcType == 1:
+        #Erosion
+        print("h")
+    elif funcType == 2:
+        #Dilation
+        print("h")
+    elif funcType == 3:
+        #Opening
+        print("h")
+    elif funcType == 4:
+        #Closing
+        print("h")
+    elif funcType == 5:
+        #Open-Close
+        print("h")
+    elif funcType == 6:
+        #Close-Open
+        print("h")
+    elif funcType == 7:
+        #Skeletanization
+        print("h")
+    elif funcType == 8:
+        #Boundary Extraction
+        print("h")
+
+
+
+def processImage(self):
+    print("processing")
+    if(imageTypeValue == 0 or funcType == 0 or inputImage == []):
+        print(imageTypeValue)
+        print(funcType)
+        print(inputImage)
+        print("alert goes here")
+        return
+
+    print("function")
+    makeGreyOrBinary()
+    print("after functions")
+
+    curDir = os.getcwd()
+
+    outputDir = '/output/'
+
+    print(curDir)
+
+    outputImageName = curDir + outputDir + "curImage" + datetime().now().strftime("%m%d-%H%M%S")+".jpg"
+    cv2.imwrite(outputImageName, inputImage)    
+
+    finalImage = ImageTk.PhotoImage(file = outputImageName)  #Image.open(imageLocation))
+    finalLabel = tk.Label(root, image = finalImage).grid(row = 31, columnspan = 6)
+    finalLabel.image = finalImage
+
+
     return
 
 
@@ -54,39 +142,46 @@ def processImage(self, event):
 root = Tk()
 
 
-Label(root, text= "Upload a binary or greyscale image").grid(row=0, column=0, sticky=W)
-
-Label(root, text= "Choose an image type:").grid(row=1, column=0, sticky=W)
+Label(root, text= "Upload a binary or greyscale image").grid(row=0, columnspan = 3, sticky=W)
 
 
+Label(root, text= "Choose an image type:").grid(row=1, columnspan = 3, sticky=W)
 binaryGreyValue = IntVar()
 #radiobuttons were set with values 1 for binary, and 2 for greyscale, stored in var v
-Radiobutton(root, text = "Binary", value = 1, variable = binaryGreyValue, command = getImageType).grid(row = 1, column = 1, sticky = W)
-Radiobutton(root, text = "Greyscale", value = 2, variable = binaryGreyValue, command = getImageType).grid(row = 1, column = 2, sticky = W)
+Radiobutton(root, text = "Binary", value = 1, variable = binaryGreyValue, command = getImageType).grid(row = 2, column = 0, sticky = W)
+Radiobutton(root, text = "Greyscale", value = 2, variable = binaryGreyValue, command = getImageType).grid(row = 2, column = 1, sticky = W)
 
 
-Label(root, text= "Choose and Image Morphology Method").grid(row=2, column=0, sticky=W)
-Radiobutton(root, text="Erosion", value=3).grid(row=3, column=0, sticky=W)
-Radiobutton(root, text="Dilation", value=4).grid(row=4, column=0, sticky=W)
+Label(root, text= "Choose an Image Morphology Method").grid(row=3, columnspan = 3, sticky=W)
+functionTypeValue = IntVar()
+Radiobutton(root, text = "Erosion", value = 1, variable = functionTypeValue, command = getFunctionType).grid(row = 4, column = 0, sticky = W)
+Radiobutton(root, text = "Dilation", value = 2, variable = functionTypeValue, command = getFunctionType).grid(row=5, column=0, sticky = W)
+Radiobutton(root, text = "Opening", value = 3, variable = functionTypeValue, command = getFunctionType).grid(row = 4, column = 1, sticky = W)
+Radiobutton(root, text = "Closing", value = 4, variable = functionTypeValue, command = getFunctionType).grid(row = 5, column = 1, sticky = W)
+Radiobutton(root, text = "Open-Close", value = 5, variable = functionTypeValue, command = getFunctionType).grid(row = 4, column = 2, sticky = W)
+Radiobutton(root, text = "Close-Open", value = 6, variable = functionTypeValue, command = getFunctionType).grid(row = 5, column = 2, sticky = W)
+Radiobutton(root, text = "Skeletinization", value = 7, variable = functionTypeValue, command = getFunctionType).grid(row = 4, column = 3, sticky = W)
+Radiobutton(root, text = "Boundary Extraction", value = 8, variable = functionTypeValue, command = getFunctionType).grid(row = 5, column = 3, sticky = W)
+
 
 
 loadImageButton = Button(root, text= "Upload Image")
 loadImageButton.bind("<Button-1>", getImage)
-loadImageButton.grid(row=5, column=0)
-
-
-
-# img = ImageTk.PhotoImage(file = "/Users/jeffsando/Desktop/Morphology_Project/cells.png")  #Image.open(imageLocation))
-# label = tk.Label(root, image=img).grid(row = 10, column = 0)
-
-
-# Button(root, text= "Process", command = getImage).grid(row=8, column=0)
+loadImageButton.grid(row=6, column=0)
 
 
 
 
-description = StringVar()
-description.set("Brief explanation of the Image Morphology method")
+
+processImageButton = Button(root, text = "Process")
+processImageButton.bind("<Button-1>", processImage)
+processImageButton.grid(row = 30, column = 0, sticky = W)
+
+
+
+
+# description = StringVar()
+# description.set("Brief explanation of the Image Morphology method")
 
 
 
